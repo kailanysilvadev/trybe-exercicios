324cd473f8682eaa28fef43bf75747592ecbf793
Kailany Silva <kailanysilva1909@gmail.com>
2023-03-28 16:33:18 -0300

    começando exercicio do dia

---
+++ fundamentos/sessao-3/dia-3.1/exercicio/scripts.js
@@ -0,0 +1,15 @@
+let a = 8;
+let b = 5;
+let c = 2;
+//exercicio 2
+
+if(a > b){
+  console.log(`O número ${a} é maior que o número ${b}`)
+}else if(b > a){
+  console.log(`O número ${b} é maior que o número ${a}`)
+}else{
+  console.log('Os números são iguais')
+}
+
+//exercicio 3
+
