c818f2011c602e62380dc43dafd95af740adc22d
Kailany Silva <kailanysilva1909@gmail.com>
2023-03-28 18:30:06 -0300

    exercicio 9 feito

--- fundamentos/sessao-3/dia-3.1/exercicio/um-par-entre-nos.js
+++ fundamentos/sessao-3/dia-3.1/exercicio/um-par-entre-nos.js
@@ -1,10 +1,19 @@
 //exercicio 8
+
 let a = 2;
 let b = 4;
-let c = 6;
+let c = 5;
 
-if(a % 2 === 0 && b % 2 === 0 && c % 2 === 0){
+if(a % 2 === 0 || b % 2 === 0 || c % 2 === 0){
   console.log(true);
 } else {
   console.log(false);
 };
+
+//exercicio 9
+
+if(a % 2 === 1 || b % 2 === 1 || c % 2 === 1){
+    console.log(true);
+  } else {
+    console.log(false);
+  };
