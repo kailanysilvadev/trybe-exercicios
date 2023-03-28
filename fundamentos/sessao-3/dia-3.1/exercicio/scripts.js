17f0f29141efa71d851219c90a78f4a5ce5607f2
Kailany Silva <kailanysilva1909@gmail.com>
2023-03-28 17:07:05 -0300

    exercicio 4 feito

--- fundamentos/sessao-3/dia-3.1/exercicio/scripts.js
+++ fundamentos/sessao-3/dia-3.1/exercicio/scripts.js
@@ -1,7 +1,8 @@
 let a = 8;
 let b = 5;
 let c = 10;
-//exercicio 2
+let num = 2;
+//exercício 2
 
 if(a > b){
   console.log(`O número ${a}-(a) é maior que o número ${b}-(b)`)
@@ -11,12 +12,20 @@ if(a > b){
   console.log('Os números são iguais')
 }
 
-//exercicio 3
+//exercício 3
 
 if(a > b && a > c){
   console.log(`o número ${a}-(a) é o maior`);
 }else if(b > a && b > c){
-  console.log(`o número ${b}-(b) é o maior`)
+  console.log(`o número ${b}-(b) é o maior`);
 }else if(c > a && c > b){
-  console.log(`o número ${c}-(c) é o maior`)
-}
+  console.log(`o número ${c}-(c) é o maior`);
+};
+
+//exercício 4
+
+if(num > 0){
+  console.log('positive');
+} else if (num < 0){
+  console.log('negative');
+};
