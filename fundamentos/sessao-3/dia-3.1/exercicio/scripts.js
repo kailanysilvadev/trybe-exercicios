6cd713604e893485ccb0e1be76b3cfcf1a8e3479
Kailany Silva <kailanysilva1909@gmail.com>
2023-03-28 18:04:55 -0300

    exercicio 6 feito

--- fundamentos/sessao-3/dia-3.1/exercicio/scripts.js
+++ fundamentos/sessao-3/dia-3.1/exercicio/scripts.js
@@ -1,4 +1,4 @@
-let a = 8;
+/*let a = 8;
 let b = 5;
 let c = 7;
 let num = 2;
@@ -36,4 +36,31 @@ if(a > 0 && b > 0 && c > 0){
   console.log(true)
 }else{
   console.log('Erro! Os ângulos do triângulo devem ser positivos')
+};*/
+
+//exercício 6
+
+let pecaXadrez = 'bispo';
+
+switch(pecaXadrez){
+  case('rei'):
+    console.log('move-se para todas as direções, mas apenas uma posição por vez');
+  break;
+  case('rainha'):
+    console.log('move-se pelas colunas, fileiras e diagonal');
+  break;
+  case('cavalo'):
+    console.log('move-se em L');
+  break
+  case('torre'):
+    console.log('move-se em direção reta pelas colunas');
+  break
+  case('peão'):
+    console.log('move-se apenas para frente, uma posição por vez (captura apenas pela diagonal)');
+  case('bispo'):
+    console.log('move-se na diagonal, quantas casas quiser');
+    break
+  default:
+     console.log('Essa não é uma peça de xadrez');
+    break
 };
