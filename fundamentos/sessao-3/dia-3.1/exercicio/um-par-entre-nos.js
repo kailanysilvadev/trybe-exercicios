06468488b2922b417dc28f64b9f881d3d157a62f
Kailany Silva <kailanysilva1909@gmail.com>
2023-03-28 19:29:31 -0300

    exercicio 10 feito

--- fundamentos/sessao-3/dia-3.1/exercicio/um-par-entre-nos.js
+++ fundamentos/sessao-3/dia-3.1/exercicio/um-par-entre-nos.js
@@ -14,6 +14,21 @@ if(a % 2 === 0 || b % 2 === 0 || c % 2 === 0){
 
 if(a % 2 === 1 || b % 2 === 1 || c % 2 === 1){
     console.log(true);
-  } else {
+} else {
     console.log(false);
-  };
+};
+
+//exercicio 10
+
+let costOfProducts = 500;
+let saleValue = 1000;
+let profit;
+
+if(costOfProducts > 0 && saleValue > 0){
+  profit = saleValue - costOfProducts;
+};
+
+console.log(profit);
+
+//exercicio 11
+
