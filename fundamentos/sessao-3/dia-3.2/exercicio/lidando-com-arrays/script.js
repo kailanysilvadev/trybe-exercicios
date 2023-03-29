43426382bc02500d370377bff77a9ac0e4ac9916
Kailany Silva <kailanysilva1909@gmail.com>
2023-03-29 17:27:50 -0300

    tópico 1 feito

--- fundamentos/sessao-3/dia-3.2/exercicio/lidando-com-arrays/script.js
+++ fundamentos/sessao-3/dia-3.2/exercicio/lidando-com-arrays/script.js
@@ -34,7 +34,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // // Tópico 5
 
-// let highestValue = 0;
+// let highestValue = numbers[0];
 
 // for(let index = 0; index < numbers.length; index +=1){
 //   if(numbers[index] > highestValue){
---
+++ fundamentos/sessao-3/dia-3.2/exercicio/logica-de-programacao/script.js
@@ -0,0 +1,9 @@
+// // Tópico 1
+// let num = 10;
+// let sum = num;
+// for(let index = 1; index < num; index +=1){
+//   sum = sum * index 
+// };
+// console.log(sum)
+
+// Tópico 2
