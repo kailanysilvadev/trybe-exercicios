c81437449ddb23aca49d5d9c96f1ef4adaa3a173
Kailany Silva <kailanysilva1909@gmail.com>
2023-03-29 15:11:09 -0300

    tópico 6 feito

--- fundamentos/sessao-3/dia-3.2/exercicio/lidando-com-arrays/script.js
+++ fundamentos/sessao-3/dia-3.2/exercicio/lidando-com-arrays/script.js
@@ -1,12 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-//Tópico 1
+// // Tópico 1
 
 // for(let index of numbers){
 //   console.log(index);
 // };
 
-//Tópico 2
+// // Tópico 2
 
 // let sum = 0;
 
@@ -15,7 +15,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // };
 // console.log(sum);
 
-//Tópico 3
+// // Tópico 3
 
 // let sum = 0;
 
@@ -25,9 +25,36 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // let arithmeticAverage = sum / numbers.length;
 //   console.log(arithmeticAverage);
 
-// //Tópico 4
+// // Tópico 4
 //  if(arithmeticAverage > 20){
 //   console.log('Valor maior que 20.');
 //  } else {
 //   console.log('Valor menor ou igual a 20');
 //  };
+
+// // Tópico 5
+
+// let highestValue = 0;
+
+// for(let index = 0; index < numbers.length; index +=1){
+//   if(numbers[index] > highestValue){
+//     highestValue = numbers[index];
+//   };
+// };
+// console.log(highestValue)
+
+// //Tópico 6
+
+// let oddValues = 0;
+
+// for(let index = 0; index < numbers.length; index +=1){
+//   if(numbers[index] % 2 !== 0){
+//     oddValues += 1;
+//   };
+// };
+// if(oddValues > 0){
+//   console.log(oddValues);
+// }else{
+//   console.log('Nenhum valor ímpar encontrado');
+// };
+
