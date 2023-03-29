a9dceb599d550e77d31b067ceacbfafcef4c0da5
Kailany Silva <kailanysilva1909@gmail.com>
2023-03-29 14:02:49 -0300

    iniciando exercicio do dia

---
+++ fundamentos/sessao-3/dia-3.2/exercicio/lidando-com-arrays/script.js
@@ -0,0 +1,22 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for(let index of numbers){
+//   console.log(index)
+// };
+
+
+// let sum = 0;
+
+// for(let index = 0; index < numbers.length; index +=1){
+//   sum += numbers[index]
+// };
+// console.log(sum)
+
+
+// let sum = 0;
+
+// for(let index = 0; index < numbers.length; index +=1){
+//   sum += numbers[index]
+// };
+// let arithmeticAverage = sum / numbers.length
+//   console.log(arithmeticAverage)
