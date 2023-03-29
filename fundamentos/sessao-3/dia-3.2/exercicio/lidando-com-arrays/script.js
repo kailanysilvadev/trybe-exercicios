a04667edd1a53ff5207fe30d61a40d511ba1a2d8
Kailany Silva <kailanysilva1909@gmail.com>
2023-03-29 14:40:36 -0300

    topico 4 feito

--- fundamentos/sessao-3/dia-3.2/exercicio/lidando-com-arrays/script.js
+++ fundamentos/sessao-3/dia-3.2/exercicio/lidando-com-arrays/script.js
@@ -1,22 +1,33 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+//Tópico 1
+
 // for(let index of numbers){
-//   console.log(index)
+//   console.log(index);
 // };
 
+//Tópico 2
 
 // let sum = 0;
 
 // for(let index = 0; index < numbers.length; index +=1){
-//   sum += numbers[index]
+//   sum += numbers[index];
 // };
-// console.log(sum)
+// console.log(sum);
 
+//Tópico 3
 
 // let sum = 0;
 
 // for(let index = 0; index < numbers.length; index +=1){
-//   sum += numbers[index]
+//   sum += numbers[index];
 // };
-// let arithmeticAverage = sum / numbers.length
-//   console.log(arithmeticAverage)
+// let arithmeticAverage = sum / numbers.length;
+//   console.log(arithmeticAverage);
+
+// //Tópico 4
+//  if(arithmeticAverage > 20){
+//   console.log('Valor maior que 20.');
+//  } else {
+//   console.log('Valor menor ou igual a 20');
+//  };
