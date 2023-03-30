4a3c37c810fa383e904ce5c88acafd983ce4cc8a
Kailany Silva <kailanysilva1909@gmail.com>
2023-03-30 16:13:13 -0300

    conteudo funções feito

---
+++ fundamentos/sessao-3/dia-3.3/conteudo/funcoes/script.js
@@ -0,0 +1,28 @@
+let sale = 200;
+let tx = 0;
+
+function addSale(value){
+  return sale + value;
+};
+
+addSale(20);
+
+
+function subtractSale(value){
+  return sale - value;
+};
+
+subtractSale(20);
+
+function multiplySale(value){
+  tx = sale * value
+  return sale += tx;
+};
+
+multiplySale(0.10);
+
+function divideSale(value){
+  return sale / value;
+};
+
+divideSale(2);
