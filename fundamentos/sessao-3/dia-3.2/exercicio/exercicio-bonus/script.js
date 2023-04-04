0efac989f779e849ba738828c3d5fa19216d0d22
Kailany Silva <kailanysilva1909@gmail.com>
2023-04-03 22:54:19 -0300

    algumas mudanças

--- fundamentos/sessao-3/dia-3.2/exercicio/exercicio-bonus/script.js
+++ fundamentos/sessao-3/dia-3.2/exercicio/exercicio-bonus/script.js
@@ -27,13 +27,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // Requisito 3
-let multiply = [];
+// let multiply = [];
 
-for(let index = 0; index < numbers.length; index += 1){
-  if (index < (numbers.length - 1)){
-    multiply.push(numbers[index] * numbers[index + 1]); 
-  } else {
-  multiply.push(numbers[index] * 2);
-  }  
-}
-console.log(multiply)
+// for(let index = 0; index < numbers.length; index += 1){
+//   if (index < (numbers.length - 1)){
+//     multiply.push(numbers[index] * numbers[index + 1]); 
+//   } else {
+//   multiply.push(numbers[index] * 2);
+//   }  
+// }
+// console.log(multiply)
