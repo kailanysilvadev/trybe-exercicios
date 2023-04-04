671b8d2544725598f6481aa03bed88cd48625a6c
Kailany Silva <kailanysilva1909@gmail.com>
2023-04-04 15:43:18 -0300

    5 requisitos feitos

--- fundamentos/sessao-3/dia-3.5/exercicio/parte-1/script.js
+++ fundamentos/sessao-3/dia-3.5/exercicio/parte-1/script.js
@@ -1,8 +1,48 @@
-// Requisito 1
+// // Requisito 1
 
-function imprimeIdade() {
-  for (let idade = 30; idade <= 40; idade += 1) {
-    console.log('Idade dentro do for:', idade)
-  }
-}
-imprimeIdade()
+// function imprimeIdade() {
+//   for (let idade = 30; idade <= 40; idade += 1) {
+//     console.log('Idade dentro do for:', idade)
+//   }
+// }
+// imprimeIdade();
+
+// // Requisito 2
+
+//   // Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
+//   const pessoa = {
+//     nome: 'Henri',
+//     idade: 20
+//   }
+//   const pessoa2 = {
+//     nome: 'Luna',
+//     idade: 19
+//   } // Altere essa estrutura para corrigir o erro.
+//   console.log('Nome:', pessoa.nome);
+//   console.log('Idade:', pessoa.idade);
+
+// // Requisito 3
+
+// let favoriteFood = 'Lasanha';
+// favoriteFood = 'Hambúrguer';
+// console.log(favoriteFood);
+
+// // Requisito 4
+
+// const name = 'Adriana';
+// const lastName = 'Soares';
+// console.log('Olá' + ',' + name + ' ' + lastName + '!');
+// function soma(a,b) {
+//   let resultado = a + b;
+//   return resultado;
+// }
+// let a = 3;
+// let b = 5;
+// console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
+
+// // Requisito 5
+
+// const numeroAleatorio = () => {
+//   return Math.random()
+// }
+// console.log(numeroAleatorio());
