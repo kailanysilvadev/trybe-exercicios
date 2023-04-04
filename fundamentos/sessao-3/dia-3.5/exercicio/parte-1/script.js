f17bc6fd7140588ddb466c3abf4a982269aace24
Kailany Silva <kailanysilva1909@gmail.com>
2023-04-04 15:55:41 -0300

    parte 1 feita completamente

--- fundamentos/sessao-3/dia-3.5/exercicio/parte-1/script.js
+++ fundamentos/sessao-3/dia-3.5/exercicio/parte-1/script.js
@@ -9,15 +9,14 @@
 
 // // Requisito 2
 
-//   // Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
+// //   // Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
 //   const pessoa = {
 //     nome: 'Henri',
 //     idade: 20
 //   }
-//   const pessoa2 = {
-//     nome: 'Luna',
-//     idade: 19
-//   } // Altere essa estrutura para corrigir o erro.
+//   pessoa['nome'] = 'Luna';
+//   pessoa['idade'] = 19;
+//  // Altere essa estrutura para corrigir o erro.
 //   console.log('Nome:', pessoa.nome);
 //   console.log('Idade:', pessoa.idade);
 
@@ -42,7 +41,25 @@
 
 // // Requisito 5
 
-// const numeroAleatorio = () => {
-//   return Math.random()
-// }
+// const numeroAleatorio = () => Math.random()
 // console.log(numeroAleatorio());
+
+// // Requisito 6
+
+// const hello = (nome) => `Olá, ${nome}!`
+// let nome = 'Ivan';
+// console.log(hello(nome));
+
+// // Requisito 7
+
+// const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome`
+// }
+// let nome = 'Ivan';
+// let sobrenome = 'Pires';
+// console.log(nomeCompleto(nome, sobrenome));
+
+// Requisito 8
+
+let speed = 185;
+const speedCar = (speed) => (speed >= 120) ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`;
+console.log(speedCar(speed));
