9c593feba901fc344dbe03cc0ff4c7a3f5376007
Kailany Silva <kailanysilva1909@gmail.com>
2023-04-10 11:28:22 -0300

    criei pasta mas não era necessario

--- fundamentos/sessao-3/dia-3.5/exercicio/parte-2/script.js
+++
@@ -1,7 +0,0 @@
-// Requisito 1
-
-const turnOnOffCar = (motorCar) => {
-  return (motorCar === 'off') ? motorCar = 'desligando motor' : motorCar = 'ligando motor'
-};
-
-console.log(turnOnOffCar('on'))
